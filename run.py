8e77262a8a13effdff8b130e7ba69315bb9d5051
Vitor Barbosa <VITORBARBOSA1301@GMAIL.COM>
2023-12-09 01:45:17 +0000

    add styling

--- run.py
+++ run.py
@@ -18,6 +18,12 @@ def contact():
     return render_template("contact.html",page_title="Contact")
 
 
+@app.route('/careers')
+def careers():
+    return render_template("careers.html",page_title="Careers")
+
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP',"0.0.0.0"),
             port=int(os.environ.get('PORT',"8080")),
